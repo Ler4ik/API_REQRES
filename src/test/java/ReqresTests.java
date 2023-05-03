20fca2255e4751d57a1e9d19adb1727c9016dffb
sanzharova <valvalsan@gmail.com>
2023-05-04 03:46:33 +0700

    added test createUser

--- src/test/java/ReqresTests.java
+++ src/test/java/ReqresTests.java
@@ -2,10 +2,10 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.is;
 
 public class ReqresTests {
-
 
     @Test
     void createUser(){
@@ -21,6 +21,41 @@ public class ReqresTests {
                 .log().status()
                 .log().body()
                 .statusCode(201)
+                .body("name", is("morpheus"))
+                .body("job", is("leader"));
+    }
+
+    @Test
+    void CreateUserWithoutJob(){
+        String data = "{ \"name\": \"morpheus\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
                 .body("name", is("morpheus"));
+    }
+
+    @Test
+    void CreateUserWithoutName(){
+        String data = "{ \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("job", is("leader"));
     }
 }
