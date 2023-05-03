321bf399a761ceb2d256203dd839bfe86916c931
sanzharova <valvalsan@gmail.com>
2023-05-04 02:00:00 +0700

    added test createUser

---
+++ src/test/java/ReqresTests.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresTests {
+
+
+    @Test
+    void createUser(){
+        String data = "{ \"name\": \"morpheus\", \"job\": \"leader\" }";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"));
+    }
+}
